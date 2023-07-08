01d4332b7652cd4578664ef3c56f1e5a577f1f9c
vu giap <vudinhgiap11@gmail.com>
2023-07-08 08:02:56 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vonglap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // vong lap hinh chu nhat
+            /*for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 7; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+            */
+            //botton left
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
